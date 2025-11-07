fb4a5a501a2c30c7b2058b45b399af42e96d5110
Jordan Cerasuolo <jcerasuo@trinity.edu>
2025-11-07 12:35:41 -0600

    Update data types

--- Connect67.hs
+++ Connect67.hs
@@ -1,12 +1,12 @@
-data Turn = Player Color deriving (Show,Eq) -- plaayer
-
 data Color = Red | Yellow deriving (Show,Eq) -- color
 
 type Board = [[Color]] -- list of columns of colors
 
-type Winner = Turn -- Player and color
+type Winner = Maybe Color -- Player and color
 
-type Move = (Integer, Turn) -- column number, color
+type Move = Integer -- column number, color
 
-makeBoard :: Board -- make empty board
+type Game = (Board, Color)
+
+makeBoard :: Board -- make empty board 
 makeBoard = [[],[],[],[],[],[],[]]
