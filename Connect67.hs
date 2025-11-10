9aa8d9f5e73f36a8c42b5e68e2752db5b76abbb4
Jordan Cerasuolo <jcerasuo@trinity.edu>
2025-11-10 13:01:46 -0600

    initial win-state check 2

--- Connect67.hs
+++ Connect67.hs
@@ -1,3 +1,5 @@
+import data.Maybe
+
 data Color = Red | Yellow deriving (Show,Eq) -- color
 
 type Board = [[Color]] -- list of columns of colors
@@ -10,3 +12,30 @@ type Game = (Board, Color)
 
 makeBoard :: Board -- make empty board 
 makeBoard = [[],[],[],[],[],[],[]]
+
+
+
+headMaybe :: [a] -> Maybe a
+headMaybe [] = Nothing
+headMaybe
+
+-- test
+
+checkWin :: Game -> Winner
+checkWin (board, color) = undefined
+
+checkVertical :: Game -> Winner
+checkVertical (board, _) = let win = catMaybes [aux column 0 | column <- board]
+                           in if null win then Nothing else head win
+                                where aux [] _ = Nothing
+                                      aux [x] counter = if counter >=4 then Just x else Nothing
+                                      aux (x:xs) counter = if counter >= 4 then Just x 
+                                                           else if x == head xs then aux xs (counter+1) else aux xs 0
+
+checkHorizontal :: Game -> Winner
+checkHorizontal (board, color) = undefined
+
+
+
+checkDiagonal :: Game -> Winner
+checkDiagonal = undefined
