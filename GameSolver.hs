0149e76196ad98109f1dc988190ed8a1ba12696e
georgemikhaelcs <mikhaelgeo477@trinity.edu>
2025-12-05 13:30:41 -0600

    took maximum out of whoWillWin and bestMove to use laziness

--- GameSolver.hs
+++ GameSolver.hs
@@ -9,25 +9,29 @@ whoWillWin :: Game -> Winner
 whoWillWin game@(board,color) = case checkWin game of
   Just x -> x
   Nothing -> if null moves then Tie
-    else scoreToWinner color (maximum (map ((scoreOutcome color . whoWillWin) . updateGame game) moves))
-  where moves = legalMoves game
-
-scoreToWinner c s
-  | s == 1  = Player c
-  | s == 0  = Tie
-  | s == -1 = if c == Red then Player Yellow else Player Red
-
-scoreOutcome c (Player winner)
-  | winner == c = 1
-  | otherwise   = -1
-scoreOutcome _ Tie = 0
+    else
+      case ((Player color) `elem` lst, Tie `elem` lst) of
+        (True, _) -> (Player color)
+        (False, True) -> Tie
+        _ -> head lst
+  where
+    lst = map (whoWillWin . updateGame game) moves
+    moves = legalMoves game
 
 --Story 10 finds a move that forces a win and returns it if it can
 --if none do, returns a move that can force a tie, if none do that either or the game is over, returns nothing
 bestMove :: Game -> Maybe Move
-bestMove game@(board,color) = if isJust (checkWin game) || bestScore == -1 then Nothing else Just move
-  where (bestScore,move) = maximum [(scoreOutcome color (whoWillWin (updateGame game move)),move)|move<-legalMoves game]
-
+bestMove game@(board,color) = if isJust (checkWin game) then Nothing else move
+  where
+    move
+      | isJust winMove = winMove
+      | isJust tieMove = tieMove
+      | otherwise = Just (head moves)
+    winMove =  lookup (Player color) lst
+    tieMove = lookup Tie lst
+    lst = [(whoWillWin (updateGame game move),move)|move<-moves]
+    moves = legalMoves game
+    
 --Story 17 return integer estimate based off game's state 
 --evaluation should return a positive value for red and negative for yellow
 
